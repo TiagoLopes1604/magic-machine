3455e94be8c74a6c112d517d1c04343fa2ca87f7
TiagoLopes1604 <160338857+TiagoLopes1604@users.noreply.github.com>
2024-03-26 14:27:08 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,12 @@ audio_bytes = audio_file.read()
 st.audio(audio_bytes, format="audio/ogg")
 
 
-st.markdown("![Alt Text](https://giphy.com/embed/l4FB6neryCg5VvsLS)")
+file_ = open("giphy.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
