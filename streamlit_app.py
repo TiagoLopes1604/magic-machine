b766961db425fb618cbec08ff601152c415f0911
TiagoLopes1604 <160338857+TiagoLopes1604@users.noreply.github.com>
2024-03-26 09:48:04 +0000

    introduçao

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,8 @@ import time
 import zipfile
 
 # Page title
-st.set_page_config(page_title='Magic Machine', page_icon='🤖')
+st.set_page_config(page_title='Magic Machine', page_icon='🧙‍♂️')
 st.title('🧙‍♂️ Magic Machine')
+
+st.markdown('**What can this app do?**')
+  st.info('"Introducing Magic Machine, your go-to companion for navigating the dynamic world of data analytics careers! With Magic Machine, new data analysts can unlock the secrets to landing their dream job in this ever-evolving industry.Providing expert insights on industry trends, Magic Machine empowers aspiring data professionals to conquer the job market with confidence. Get ready to embark on your career journey with Magic Machine – where data meets destiny!')
