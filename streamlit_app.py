62fbb233b52229bb8c1f91f2c3b41122c3dc0dce
TiagoLopes1604 <160338857+TiagoLopes1604@users.noreply.github.com>
2024-03-27 09:24:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,14 @@ import pickle
 st.set_page_config(page_title='Magic Machine', page_icon='🧙‍♂️')
 st.title('🧙‍♂️ Magic Machine')
 
+
+
+st.markdown('**What can this app do?**')
+st.info('"Introducing Magic Machine, your go-to companion for navigating the dynamic world of data analytics careers! With Magic Machine, new data analysts can unlock the secrets to landing their dream job in this ever-evolving industry.Providing expert insights on industry trends, Magic Machine empowers aspiring data professionals to conquer the job market with confidence. Get ready to embark on your career journey with Magic Machine – where data meets destiny!')
+
+audio_file = open("PiratesOfTheCaribbeanThemeSong.mp3", "rb")
+audio_bytes = audio_file.read()
+st.audio(audio_bytes, format="audio/ogg")
 st.markdown(
     """
     <style>
@@ -23,14 +31,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-st.markdown('**What can this app do?**')
-st.info('"Introducing Magic Machine, your go-to companion for navigating the dynamic world of data analytics careers! With Magic Machine, new data analysts can unlock the secrets to landing their dream job in this ever-evolving industry.Providing expert insights on industry trends, Magic Machine empowers aspiring data professionals to conquer the job market with confidence. Get ready to embark on your career journey with Magic Machine – where data meets destiny!')
-
-audio_file = open("PiratesOfTheCaribbeanThemeSong.mp3", "rb")
-audio_bytes = audio_file.read()
-st.audio(audio_bytes, format="audio/ogg")
-
 
 
 import base64
